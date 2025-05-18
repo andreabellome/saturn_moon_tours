b0b6428736bbddb25f97296d00edde55912afc04
andreabellome <andrea.bellome@gmail.com>
2025-05-18 20:19:04 +0200

    Update st0_2_plot_tisserand_poincare_graph.m

--- st0_2_plot_tisserand_poincare_graph.m
+++ st0_2_plot_tisserand_poincare_graph.m
@@ -76,10 +76,13 @@ close all; clc;
 
 % --> load the Task 2 solution
 load([pwd '/Solutions/PATHph_noOpCon.mat']);
+load([pwd '/crossing_data.mat']);
 
 % --> Enceladus phase
 tour   = PATHph(end).path;
 idMoon = tour(1,1);
+INPUT.vinflevels = vinfLevDIM;
+INPUT.idcentral  = idcentral;
 
 % --> add all the contours
 idcentral  = 6; % --> central body (Saturn in this case)
@@ -91,8 +94,16 @@ vinfLevDIM = 0.2:0.05:0.8;     % --> infinity velocity levels [km/s]
 
 % --> plot bits
 fig3       = plot_tp_graph(idcentral, idMoon, vinfLevDIM, ramaxAdim, npoints, param); % --> plot TP graph
-plotContours( idMoon, vinfLevDIM, idcentral, 1 );                                        % --> plot v-infinity graph
-plotContours( 2, 0.8, idcentral, 1 );                                        % --> plot v-infinity graph
+plotContours( idMoon, vinfLevDIM, idcentral, 1 );                                     % --> plot v-infinity graph
+plotContours( 2, 0.8, idcentral, 1 );                                                 % --> plot v-infinity graph
 
 processLabelPlots( idcentral, idMoon );                                               % --> adjust the lables and scaling
-plotPath_reduced( tour, idcentral, 'b' );                                             % --> plot the tour
+plotPath_tiss(tour, INPUT, 'b', [], [], [], [], 1);                                   % --> plot the tour
+
+% --> plot the Poincarè-crossing orbits
+if idcentral == 1
+    AU = 149597870.7;
+else
+    [~, AU] = planetConstants(idcentral);
+end
+plot( RA_CROSSING_DIM./AU, RP_CROSSING_DIM./AU, 'o', 'MarkerSize', 5, 'MarkerEdgeColor', 'black', 'MarkerFaceColor', 'yellow' );
