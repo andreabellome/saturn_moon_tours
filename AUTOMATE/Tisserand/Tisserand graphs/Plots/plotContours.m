4005c44906da9c4fb69aa77681ba7495195e3514
andreabellome <andrea.bellome@gmail.com>
2025-06-17 09:17:42 +0200

    Update plotContours.m

--- AUTOMATE/Tisserand/Tisserand graphs/Plots/plotContours.m
+++ AUTOMATE/Tisserand/Tisserand graphs/Plots/plotContours.m
@@ -58,8 +58,10 @@ elseif nargin == 6
 end
 
 if holdon == 1 % --> keep the same figure open
+    fig = gcf;
+    fig.Color = [ 1 1 1 ];
 else
-    figure( 'Color', [0 0 0] );
+    figure( 'Color', [1 1 1] );
 end
 
 if isempty(AU)
@@ -82,7 +84,7 @@ for indplanet = 1:length(IDS)
 
     [~, fullName] = planet_names_GA(idpl, idcentral);
 
-    hold on;
+    hold on; grid on;
     for indi = 1:length(vinflevels)
 
         [rascCONT, rpscCONT] = ...
@@ -137,96 +139,7 @@ for indplanet = 1:length(IDS)
     end
 end
 
-if idcentral == 1
-
-    idMIN = min(IDS);
-    idMAX = max(IDS);
-    XLIM(:,1) = [0.15 0.7 0.9 2 4 9.7 19]';
-    XLIM(:,2) = [0.5  2   2.8 6 6 15  30]';  
-    xlim([XLIM(idMIN,1) XLIM(idMAX,2)]);
-    if idMAX == 5
-        ylim([0 3]);
-    elseif idMAX == 6
-        ylim([0 4]);
-    elseif idMAX == 7
-        ylim([0 6.5]);
-    end
-    
-    xlabel('r_a [AU]'); ylabel('r_p [AU]');
-
-elseif idcentral == 5
-
-    if max(IDS) == 4
-        ylim([0 30]);
-        xlim([5 200]);
-    elseif max(IDS) == 3
-        ylim([0 16]);
-        xlim([5 200]);
-    end
-
-    xlabel('r_a [R_J]'); ylabel('r_p [R_J]');
-
-elseif idcentral == 6
-
-    if (max(IDS) == 5 && min(IDS) == 4) || ( max(IDS) == 5 && length(IDS) == 1 )
-
-        ylim([2 22]);
-        xlim([20 80]);
-
-%         ylim([8 22]);
-%         xlim([20 350]);
-    
-    elseif max(IDS) == 5 && min(IDS) == 0
-
-        ylim([1.5 9.5]);
-        xlim([3.8 35.5]);
-
-    elseif max(IDS) == 5 && min(IDS) == 1
-
-        ylim([1.5 9.5]);
-        xlim([3.8 35.5]);
-
-
-    elseif max(IDS) == 4 && min(IDS) == 3
-
-%         ylim([2 9.5]);
-%         xlim([5 26]);
-
-        ylim([6 9.5]);
-        xlim([8 24.5]);
-
-    elseif max(IDS) == 3 && min(IDS) == 2
-
-        xlim([6.4 9.8]);
-        ylim([4.8 6.5]);
-
-    elseif max(IDS) == 2 && min(IDS) == 1
-
-        xlim([5 6.8]);
-        ylim([3.8 5.1]);
-
-    elseif max(IDS) == 1
-
-        ylim([4 4.12]);
-        xlim([4 5.2]);
-
-    elseif max(IDS) == 4 && length(IDS) == 1
-        
-        ylim([5.5 9.5]);
-        xlim([8.5 40.0]);
-
-    end
-
-    xlabel('r_a [R_S]'); ylabel('r_p [R_S]');
-
-elseif idcentral == 7
-
-%     ylim();
-    xlim([0 150]);
-
-    xlabel('r_a [R_U]'); ylabel('r_p [R_U]');
-
-end
+processLabelPlots( idcentral, IDS );
 
 fig = gcf;
 fig.Color = [1 1 1];
